56a6bd0e52230d4d002c2a1d7a0c6248782ac657
semanticker <semanticker@gmail.com>
2022-05-08 00:09:07 +0900

    양방향 연관관계와 연관관계의 주인 - 주의점

--- ex3-member-team/src/main/java/com/example/bidir/JpaMain.java
+++ ex3-member-team/src/main/java/com/example/bidir/JpaMain.java
@@ -9,7 +9,120 @@ import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
-        bidirectional();
+        // 양방향
+        //bidirectional();
+
+        // 실수하기 쉬운 패턴
+        //do_mistake();
+
+        // 매핑의 주인으로 실행
+        //do_right();
+        
+        // 양쪽다 매핑
+        do_dual();
+
+    }
+
+    private static void do_dual() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.changeTeam(team);
+            em.persist(member);
+
+            //team.getMembers().add(member);
+
+            //em.flush();
+            //em.clear();
+
+            Team findTeam = em.find(Team.class, team.getId());
+            List<Member> members = findTeam.getMembers();
+
+            System.out.println("=====");
+            for (Member m : members) {
+                System.out.println("m.getUsername() = " + m.getUsername());
+            }
+            System.out.println("=====");
+
+            tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            tx.rollback();
+        } finally {
+            em.close();
+            emf.close();
+        }
+    }
+
+    private static void do_right() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setTeam(team);
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            tx.rollback();
+        } finally {
+            em.close();
+            emf.close();
+        }
+    }
+
+    private static void do_mistake() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Member member = new Member();
+            member.setUsername("member1");
+            em.persist(member);
+
+            Team team = new Team();
+            team.setName("TeamA");
+            team.getMembers().add(member);
+            em.persist(team);
+
+            em.flush();
+            em.clear();
+
+            tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            tx.rollback();
+        } finally {
+            em.close();
+            emf.close();
+        }
     }
 
     private static void bidirectional() {
--- ex3-member-team/src/main/java/com/example/bidir/Member.java
+++ ex3-member-team/src/main/java/com/example/bidir/Member.java
@@ -39,4 +39,9 @@ public class Member {
     public void setTeam(Team team) {
         this.team = team;
     }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
 }
