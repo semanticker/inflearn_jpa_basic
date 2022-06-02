5cb201274cd8a2e9350e9d66d2e852677ee019e8
semanticker <semanticker@gmail.com>
2022-06-02 22:29:24 +0900

    객체지향 쿼리 언어 - JPQL, Criteria

---
+++ ex7/src/main/java/hellojpa/criteriabuilder/JpaMain.java
@@ -0,0 +1,58 @@
+package hellojpa.criteriabuilder;
+
+
+import hellojpa.start.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Member> query = cb.createQuery(Member.class);
+
+            Root<Member> m = query.from(Member.class);
+
+            CriteriaQuery<Member> select = query.select(m)
+                            .where(cb.equal(m.get("username"), "kim"));
+            List<Member> resultList = em.createQuery(select)
+                    .getResultList();
+
+
+            CriteriaQuery<Member> select2 = query.select(m)
+                    .where(cb.equal(m.get("username"), "kim"));
+
+            String username = "";
+
+            // 동적 쿼리 > 조건
+            if (username != null) {
+                select2 = select2.where(cb.equal(m.get("username"), "kim"));
+            }
+
+            List<Member> resultList2 = em.createQuery(select2)
+                    .getResultList();
+
+            tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            tx.rollback();
+        } finally {
+            em.close();
+            emf.close();
+        }
+    }
+}
