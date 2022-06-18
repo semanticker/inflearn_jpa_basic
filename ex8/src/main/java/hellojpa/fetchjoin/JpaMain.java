6ffc317664e79c9a170bf5841d8f1147dbc2baee
semanticker <semanticker@gmail.com>
2022-06-18 20:47:45 +0900

    페치 조인2 - 한계 @BatchSize(size = 100) hibernate.default_batch_fetch_size

--- ex8/src/main/java/hellojpa/fetchjoin/JpaMain.java
+++ ex8/src/main/java/hellojpa/fetchjoin/JpaMain.java
@@ -1,6 +1,8 @@
 package hellojpa.fetchjoin;
 
 
+import org.hibernate.annotations.BatchSize;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -43,27 +45,27 @@ public class JpaMain {
             em.clear();
 
             // Member 검색후 필요할때 Team 검색
-            String query = "select m from hellojpa.fetchjoin.Member m";
+            /*String query = "select m from hellojpa.fetchjoin.Member m";
 
             List<Member> resultList = em.createQuery(query, Member.class)
                     .getResultList();
 
             for (Member member: resultList) {
                 System.out.println("member = " + member.getUsername() + ", " + member.getTeam().getName());
-            }
+            }*/
 
             // fetch join
-            String query2 = "select m from hellojpa.fetchjoin.Member m join fetch m.team";
+            /*String query2 = "select m from hellojpa.fetchjoin.Member m join fetch m.team";
 
             List<Member> resultList2 = em.createQuery(query2, Member.class)
                     .getResultList();
 
             for (Member member: resultList2) {
                 System.out.println("member = " + member.getUsername() + ", " + member.getTeam().getName());
-            }
+            }*/
 
             // fetch join
-            String query3 = "select t from hellojpa.fetchjoin.Team t join fetch t.members";
+            /*String query3 = "select t from hellojpa.fetchjoin.Team t join fetch t.members";
 
             List<Team> resultList3 = em.createQuery(query3, Team.class)
                     .getResultList();
@@ -74,26 +76,26 @@ public class JpaMain {
                     System.out.println("    member = " + member);
 
                 }
-            }
+            }*/
 
             // distinct
-            String query4 = "select distinct t from hellojpa.fetchjoin.Team t join fetch t.members";
+            /*String query4 = "select distinct t from hellojpa.fetchjoin.Team t join fetch t.members";
 
             List<Team> resultList4 = em.createQuery(query4, Team.class)
                     .getResultList();
 
-            System.out.println("resultList4 = " + resultList4.size());
+            System.out.println("resultList4 = " + resultList4.size());*/
 
 
             // 페이징 : 이렇게 처리하면 안됨.
-            String query5 = "select  t from hellojpa.fetchjoin.Team t join fetch t.members ㅡ";
+            /*String query5 = "select  t from hellojpa.fetchjoin.Team t join fetch t.members ㅡ";
 
             List<Team> resultList5 = em.createQuery(query5, Team.class)
                     .setFirstResult(0)
                     .setMaxResults(1)
                     .getResultList();
 
-            System.out.println("resultList5 = " + resultList5.size());
+            System.out.println("resultList5 = " + resultList5.size());*/
 
             // 팀 조회 : 멤버를 레이지로 로딩 => 성능이 안나옴
             String query6 = "select  t from hellojpa.fetchjoin.Team t";
@@ -112,7 +114,10 @@ public class JpaMain {
                 }
             }
 
-
+            // Team의 @OneToMany 의 members에 @BatchSize(size = 100) 속성 부여하면
+            // Members 대상이 되는 팀조건을 members0_.TEAM_ID in (?, ?) 추가하여 가져옴
+            // @BatchSize(size = 100) 속성은 persistence.xml 에 hibernate.default_batch_fetch_size 속성을 이용하여
+            // 전역으로 설정할수 있음
 
             tx.commit();
         } catch (Exception e) {
--- ex8/src/main/java/hellojpa/fetchjoin/Team.java
+++ ex8/src/main/java/hellojpa/fetchjoin/Team.java
@@ -1,5 +1,7 @@
 package hellojpa.fetchjoin;
 
+import org.hibernate.annotations.BatchSize;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +15,7 @@ public class Team {
     private Long id;
     private String name;
 
+    @BatchSize(size = 100)
     @OneToMany(mappedBy = "team")
     private List<Member> members = new ArrayList<>();
 
